26c2be3e4ed387d52bfe4c697f72a9001b0bb656
Wouter Wijsman <wouter@gridscale.io>
2024-10-23 12:00:59 +0200

    Make get_depends actually look at directory names

--- get_depends.py
+++ get_depends.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 import json
+import os
 
 if __name__ == '__main__':
-    packages = [
-        "nvidia-utils",
-        "lib32-nvidia-utils"
-    ]
+    packages = []
+    for file in os.listdir():
+        if os.path.isdir(file) and not file.startswith("."):
+            packages.append(file)
     print(json.dumps(packages))
